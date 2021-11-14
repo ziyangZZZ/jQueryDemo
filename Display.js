9ca19628deeb6db0654bd971279828ef6ebfeb79
chaotong <chaotong@qq.com>
2021-11-14 19:53:09 +0800

    feat: 隐藏与显示

--- Display.js
+++ Display.js
@@ -1,2 +1,20 @@
 // console.log('$',$());
+// console.log('name',$('[name = show_hide]'));
 
+$(function () {
+    showHide();
+
+    function showHide() {
+        $('[name = show_hide]').hover(
+            function () {
+                var $id = this.id + '_items'
+                $('#' + $id).show();
+            },
+            function () {
+                var $id = this.id + '_items'
+                $('#' + $id).hide();
+            }
+        )
+
+    }
+})
